cc6af041c3564439416833844c232ef428ab6ff7
Stefano Oliveri <stefano.oliveri@stf12.net>
2020-08-23 18:51:00 +0200

    WARNING!: FreeRTOS patch for critical section.
    disable the IRQs instead of masking only part of them.

--- ODeV_f/system/src/freertos/portable/GCC/ARM_CM4F/portmacro.h
+++ ODeV_f/system/src/freertos/portable/GCC/ARM_CM4F/portmacro.h
@@ -98,8 +98,14 @@ extern void vPortEnterCritical( void );
 extern void vPortExitCritical( void );
 #define portSET_INTERRUPT_MASK_FROM_ISR()		ulPortRaiseBASEPRI()
 #define portCLEAR_INTERRUPT_MASK_FROM_ISR(x)	vPortSetBASEPRI(x)
-#define portDISABLE_INTERRUPTS()				vPortRaiseBASEPRI()
-#define portENABLE_INTERRUPTS()					vPortSetBASEPRI(0)
+//TODO: STF.Begin
+//#define portDISABLE_INTERRUPTS()        vPortRaiseBASEPRI()
+//#define portENABLE_INTERRUPTS()             vPortSetBASEPRI(0)
+//#define portDISABLE_INTERRUPTS()          __asm volatile ("MSR basepri, %0" : : "r" (configMAX_SYSCALL_INTERRUPT_PRIORITY) : "memory");
+//#define portENABLE_INTERRUPTS()           __asm volatile ("MSR basepri, %0" : : "r" (0) : "memory");
+#define portDISABLE_INTERRUPTS()                __asm volatile ("cpsid i" : : : "memory");
+#define portENABLE_INTERRUPTS()                 __asm volatile ("cpsie i" : : : "memory");__asm volatile ("isb 0xF":::"memory");
+//STF.End
 #define portENTER_CRITICAL()					vPortEnterCritical()
 #define portEXIT_CRITICAL()						vPortExitCritical()
 
